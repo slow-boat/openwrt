7d52c92e23014ba4544569db17bdd358a5e6d4e8
Chris Lockwood <chris.lockwood@avi.com.au>
2021-11-22 16:19:46 +0800

    updated dhcp options

--- package/network/config/netifd/files/lib/netifd/proto/dhcp.sh
+++ package/network/config/netifd/files/lib/netifd/proto/dhcp.sh
@@ -25,6 +25,16 @@ proto_dhcp_init_config() {
 	proto_config_add_string mtu6rd
 	proto_config_add_string customroutes
 	proto_config_add_boolean classlessroute
+	proto_config_add_boolean ipv6
+	proto_config_add_boolean defaultroute
+	proto_config_add_boolean usegateway
+	proto_config_add_boolean subnetroute
+	proto_config_add_boolean dns
+	proto_config_add_boolean peerdns
+	proto_config_add_string metric
+	proto_config_add_string ip4table
+	proto_config_add_string ip6table
+	proto_config_add_string table
 }
 
 proto_dhcp_add_sendopts() {
@@ -35,8 +45,8 @@ proto_dhcp_setup() {
 	local config="$1"
 	local iface="$2"
 
-	local ipaddr hostname clientid vendorid broadcast norelease reqopts defaultreqopts iface6rd sendopts delegate zone6rd zone mtu6rd customroutes classlessroute
-	json_get_vars ipaddr hostname clientid vendorid broadcast norelease reqopts defaultreqopts iface6rd delegate zone6rd zone mtu6rd customroutes classlessroute
+	local ipaddr hostname clientid vendorid broadcast norelease reqopts defaultreqopts iface6rd sendopts delegate zone6rd zone mtu6rd customroutes classlessroute ipv6 defaultroute usegateway subnetroute dns peerdns metric table ip4table ip6table
+	json_get_vars ipaddr hostname clientid vendorid broadcast norelease reqopts defaultreqopts iface6rd delegate zone6rd zone mtu6rd customroutes classlessroute ipv6 defaultroute usegateway subnetroute dns peerdns metric table ip4table ip6table
 
 	local opt dhcpopts
 	for opt in $reqopts; do
@@ -44,6 +54,7 @@ proto_dhcp_setup() {
 	done
 
 	json_for_each_item proto_dhcp_add_sendopts sendopts dhcpopts
+	[ "$ipv6" = "1" ] || ipv6=
 
 	[ -z "$hostname" ] && hostname="$(cat /proc/sys/kernel/hostname)"
 	[ "$hostname" = "*" ] && hostname=
@@ -52,21 +63,35 @@ proto_dhcp_setup() {
 	[ "$broadcast" = 1 ] && broadcast="-B" || broadcast=
 	[ "$norelease" = 1 ] && norelease="" || norelease="-R"
 	[ -n "$clientid" ] && clientid="-x 0x3d:${clientid//:/}" || clientid="-C"
-	[ -n "$iface6rd" ] && proto_export "IFACE6RD=$iface6rd"
-	[ "$iface6rd" != 0 -a -f /lib/netifd/proto/6rd.sh ] && append dhcpopts "-O 212"
-	[ -n "$zone6rd" ] && proto_export "ZONE6RD=$zone6rd"
+	
+	[ -z "$ipv6" ] && {
+		[ -n "$iface6rd" ] && proto_export "IFACE6RD=$iface6rd"
+		[ "$iface6rd" != 0 -a -f /lib/netifd/proto/6rd.sh ] && append dhcpopts "-O 212"
+		[ -n "$zone6rd" ] && proto_export "ZONE6RD=$zone6rd"
+		[ -n "$mtu6rd" ] && proto_export "MTU6RD=$mtu6rd"
+		[ "$delegate" = "0" ] && proto_export "IFACE6RD_DELEGATE=0"
+	}
 	[ -n "$zone" ] && proto_export "ZONE=$zone"
-	[ -n "$mtu6rd" ] && proto_export "MTU6RD=$mtu6rd"
 	[ -n "$customroutes" ] && proto_export "CUSTOMROUTES=$customroutes"
-	[ "$delegate" = "0" ] && proto_export "IFACE6RD_DELEGATE=0"
+	[ -n "$dns" ] && proto_export "DNSOVERRIDE=$dns"
+	[ "$defaultroute" = "0" ] && proto_export "DEFAULTROUTE=0"
+	[ "$usegateway" = "0" ] && proto_export "USEGATEWAY=0"
+	[ "$subnetroute" = "0" ] && proto_export "SUBNETROUTE=0"
+	[ "$peerdns" = "0" ] && proto_export "PEERDNS=0"
+	
 	# Request classless route option (see RFC 3442) by default
 	[ "$classlessroute" = "0" ] || append dhcpopts "-O 121"
 
+	[ -n "$metric" ] && proto_export "METRIC=$metric"
+	[ -n "$table" ] && proto_export "IP4TABLE=$table" && [ "$ipv6" = "1" ] && proto_export "IP6TABLE=$table"
+	[ -n "$ip4table" ] && proto_export "IP4TABLE=$ip4table"
+	[ -n "$ip6table" ] && [ "$ipv6" = "1" ] && proto_export "IP6TABLE=$ip6table"
+	
 	proto_export "INTERFACE=$config"
 	proto_run_command "$config" udhcpc \
 		-p /var/run/udhcpc-$iface.pid \
 		-s /lib/netifd/dhcp.script \
-		-f -t 0 -i "$iface" \
+		-f -R -t 0 -i "$iface" \
 		${ipaddr:+-r $ipaddr} \
 		${hostname:+-x "hostname:$hostname"} \
 		${vendorid:+-V "$vendorid"} \
